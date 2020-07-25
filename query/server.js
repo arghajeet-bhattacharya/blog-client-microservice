d7646432addf6410402749db4516ff4d058e2e5c
DamionDarhk <DamionDarhk@github.com>
2020-07-26 00:41:19 +0530

    added flow to reach event bus history in case if an event is missed

--- query/server.js
+++ query/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require("axios");
 
 const app = express();
 
@@ -9,6 +10,31 @@ app.use(cors());
 
 const posts = {};
 
+const handleEvents = (type, data) => {
+    if(type === 'post_created') {
+        const {id, title} = data;
+        posts[id] = {id, title, comments: []}
+    }
+
+    if(type === 'comment_created') {
+        const {id, content, postId, status} = data;
+        const post = posts[postId];
+
+        post.comments.push({id, content, status});
+    }
+
+    if(type === 'comment_updated') {
+        const {id, postId, status, content} = data;
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+        comment.content = content;
+    }
+}
+
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
@@ -16,21 +42,18 @@ app.get('/posts', (req, res) => {
 app.post('/events', (req, res) => {
     const {type, data} = req.body;
 
-    if(type === 'post_created') {
-        const {id, title} = data;
-        posts[id] = {id, title, comments: []}
-    }
-
-    if(type === 'comment_created') {
-        const {id, content, postId} = data;
-        const post = posts[postId];
-
-        post.comments.push({id, content});
-    }
+    handleEvents(type, data);
 
     res.send({message: "Event Received"});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.info("Listening on port: 4002");
+
+    const eventData = await axios.get("http://localhost:4005/events");
+
+    for(let singleEvent of eventData.data) {
+        console.info("processinng event: ", singleEvent.type);
+        handleEvents(singleEvent.type, singleEvent.data);
+    }
 })
