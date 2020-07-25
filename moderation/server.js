bb4f074928432fa8bb584d939e190b649e829aff
DamionDarhk <DamionDarhk@github.com>
2020-07-26 00:42:26 +0530

    add moderation service

---
+++ moderation/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body;
+
+    if(type === 'comment_created') {
+        const {status, id, postId, content} = data;
+        const moderatedStatus = content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'comment_moderated',
+            data: {
+                id,
+                postId,
+                content,
+                status: moderatedStatus
+            }
+        })
+
+    }
+
+    res.send({message: "Event Received"});
+});
+
+app.listen(4003, () => {
+    console.info("Listening on port: 4003");
+})
